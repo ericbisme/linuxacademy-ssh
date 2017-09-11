b18fa0d1e5bb838aed3a6fe7b7fa15f1fedbf2a2
root <root@ericbisme1.mylabserver.com>
2017-09-11 00:59:22 +0000

    updates from lessons, prep for lab

---
+++ manifests/params.pp
@@ -0,0 +1,16 @@
+class ssh::params {
+  case $facts['os']['family'] {
+    'Debian': {
+      $package_name = 'openssh-server'
+      $service_name = 'ssh'
+    }
+    'RedHat': {
+      $package_name = 'openssh-server'
+      $service_name = 'sshd'
+    }
+    default: {
+      fail("${$facts['os']['family']} is not supported!")
+    }
+  }
+}
+
